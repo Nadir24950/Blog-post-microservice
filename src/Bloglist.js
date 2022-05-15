8aa2b10661942b808466cf5fab88f79e384be0b3
Nadir <ndir24950@gmail.com>
2022-05-16 00:52:14 +0200

    small changes

--- src/Bloglist.js
+++ src/Bloglist.js
@@ -1,19 +1,19 @@
 
-const Bloglist = (props) => {
-    const blogs = props.blogs; 
-    return (  
-            <div>
-                {
-                    blogs.map((blog) => (
-                        <div className="blog-preview" key={blog.id}>
-                            <h3>{blog.title}</h3>
-                            <p>by {blog.author}</p>
-                            <p>{blog.content}</p>
-                        </div>
-                    ))
-                }
-            </div>
+const Bloglist = ({blogs, title, handleDelete}) => {
+    return (
+        <div>
+            <h3>{title}</h3>
+            {blogs.map((blog) => (
+                    <div className="blog-preview" key={blog.id}>
+                        <h3>{blog.title}</h3>
+                        <p>by {blog.author}</p>
+                        <p>{blog.content}</p>
+                        <button onClick={() => handleDelete(blog.id)}>Delete blog</button>
+                    </div>
+                ))
+            }
+        </div>
     );
 }
- 
+
 export default Bloglist;
--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,25 @@
 import Bloglist from "./Bloglist";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
         {"title": "Welcome to my blog", "content": "this is the content of my blog " , "author": "Nadir", "id": 1},
-        {"title": "First blog", "content": "This is my first blog" , "author": "Nadir", "id": 2}
+        {"title": "First blog", "content": "This is my first blog" , "author": "Nadir", "id": 2},
+        {"title": "Sabrina's blog", "content": "This is my first blog" , "author": "Sabrina", "id": 3}
     ])
 
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter((blog) => blog.id !== id)
+        setBlogs(newBlogs);
+    }
+
+    useEffect(() => {
+        console.log('Hi there');
+    });
     return (
         <div className="home">
             <h2>Homepage </h2>
-            <Bloglist blogs={blogs}></Bloglist>
+            <Bloglist blogs={blogs} title="All blogs" handleDelete={handleDelete}></Bloglist>
         </div>
     );
 }
