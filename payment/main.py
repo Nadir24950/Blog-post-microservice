1eae07ad5b10d1245295d8f33e0a0242725ac2e2
Nadir24950 <nadir24950@gmail.com>
2023-08-15 22:04:03 +0200

    changed the name from inventory to blogs

--- payment/main.py
+++ payment/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['http://localhost:3000'],
+    allow_origins=['http://localhost:5173'],
     allow_methods=['*'],
     allow_headers=['*']
 )
